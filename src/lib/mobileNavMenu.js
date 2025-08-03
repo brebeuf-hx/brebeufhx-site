c94922a2be4ce172d4e4ce51fcb2dedc9e7005d1
Aidan T <botatooo@protonmail.com>
2025-08-03 13:47:49 +0300

    feat: fix hamburger menu on mobile, change backgrounds that were plain white, fix translation, fix sponsor section and add sample sponsors, remove easter egg (cus it was bad), fix footer social media icons

--- src/lib/mobileNavMenu.js
+++ src/lib/mobileNavMenu.js
@@ -1,17 +1,33 @@
+import { addNavbarBackground, removeNavbarBackground } from "./navbar.js";
+
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
+export let hamburgerIsOpen = false;
+
 hamburger.addEventListener('click', () => {
+  hamburgerIsOpen = !hamburgerIsOpen;
+
+  if (hamburgerIsOpen) {
+    addNavbarBackground(true);
+  } else if (window.scrollY <= 50) {
+    removeNavbarBackground();
+  }
+
+  // existing classes
   navMenu.classList.toggle('hidden');
+  navMenu.classList.toggle('space-x-8');
+  navMenu.classList.toggle('md:flex');
+  // mobile menu classes
   navMenu.classList.toggle('flex');
   navMenu.classList.toggle('flex-col');
   navMenu.classList.toggle('absolute');
   navMenu.classList.toggle('top-full');
   navMenu.classList.toggle('left-0');
   navMenu.classList.toggle('right-0');
-  navMenu.classList.toggle('bg-white');
-  navMenu.classList.toggle('shadow-lg');
-  navMenu.classList.toggle('p-4');
   navMenu.classList.toggle('space-y-4');
-  navMenu.classList.toggle('space-x-8');
+  navMenu.classList.toggle('p-4');
+  navMenu.classList.toggle('bg-white/95');
+  navMenu.classList.toggle('backdrop-blur-md');
+  navMenu.classList.toggle('shadow-lg');
 });
--- src/lib/navbar.js
+++ src/lib/navbar.js
@@ -1,3 +1,5 @@
+import { hamburgerIsOpen } from "./mobileNavMenu.js";
+
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 const navbar = document.querySelector('#navbar');
@@ -5,35 +7,75 @@ const logo = document.querySelector('#logo');
 const hx = document.querySelector('#hx');
 const langBtn = document.querySelector('#lang-btn');
 
+const navLinks = Array.from(navMenu.querySelectorAll('a'))
+const hamburgerLayers = Array.from(hamburger.querySelectorAll('span'));
+
+// to avoid those bugs caused by scrolling, and then opening and closing the hamburger menu on mobile etc. so basically redundancy
+let navbarBackgroundWasAdded = false;
+let navbarBackgroundWasRemoved = true;
+
+export function addNavbarBackground(mobileMenu) {
+    if (navbarBackgroundWasAdded) {
+        console.log('already there, quitting')
+        return;
+    }
+    navbarBackgroundWasAdded = true;
+    navbarBackgroundWasRemoved = false;
+
+    navbar.classList.add('bg-white/95', 'backdrop-blur-md');
+    if (!mobileMenu) {
+        navbar.classList.add('border-b-2', 'border-ice-blue-100');
+    }
+    logo.classList.add('text-gray-800');
+    logo.classList.remove('text-white');
+
+    hamburgerLayers.forEach((l) => {
+        l.classList.add('bg-gray-800');
+        l.classList.remove('bg-white');
+    });
+    navLinks.forEach((l) => {
+        l.classList.add('text-gray-800', 'hover:text-ice-blue-500');
+        l.classList.remove('text-gray-200', 'hover:text-white');
+    });
+
+    hx.classList.add('from-ice-blue-400','to-ice-blue-600');
+    hx.classList.remove('from-ice-blue-100','to-ice-blue-200');
+    langBtn.classList.add('bg-ice-blue-500', 'text-white');
+    langBtn.classList.remove('bg-white', 'text-ice-blue-500');
+}
+
+export function removeNavbarBackground() {
+    if (navbarBackgroundWasRemoved) {
+        return;
+    }
+    navbarBackgroundWasRemoved = true;
+    navbarBackgroundWasAdded = false;
+
+    navbar.classList.remove('bg-white/95', 'backdrop-blur-md', 'border-b-2', 'border-ice-blue-100');
+    logo.classList.add('text-white');
+    logo.classList.remove('text-gray-800');
+
+    hamburgerLayers.forEach((l) => {
+        l.classList.add('bg-white');
+        l.classList.remove('bg-gray-800');
+    });
+    navLinks.forEach((l) => {
+        l.classList.add('text-gray-200', 'hover:text-white');
+        l.classList.remove('text-gray-800', 'hover:text-ice-blue-500');
+    });
+
+    hx.classList.add('from-ice-blue-100','to-ice-blue-200');
+    hx.classList.remove('from-ice-blue-400','to-ice-blue-600');
+    langBtn.classList.add('bg-white', 'text-ice-blue-500');
+    langBtn.classList.remove('bg-ice-blue-500', 'text-white');
+}
+
 // Navbar background //
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 50) {
-      navbar.classList.add('bg-white/95', 'backdrop-blur-md', 'border-b-2', 'border-ice-blue-100');
-      logo.classList.add('text-gray-800');
-      logo.classList.remove('text-white');
-
-      navMenu.querySelectorAll('a').forEach(a => {
-          a.classList.add('text-gray-800', 'hover:text-ice-blue-500');
-          a.classList.remove('text-gray-200', 'hover:text-white');
-      });
-
-      hx.classList.add('from-ice-blue-400','to-ice-blue-600');
-      hx.classList.remove('from-ice-blue-100','to-ice-blue-200');
-      langBtn.classList.add('bg-ice-blue-500', 'text-white');
-      langBtn.classList.remove('bg-white', 'text-ice-blue-500');
-  } else {
-      navbar.classList.remove('bg-white/95', 'backdrop-blur-md', 'border-b-2', 'border-ice-blue-100');
-      logo.classList.add('text-white');
-      logo.classList.remove('text-gray-800');
-      
-      navMenu.querySelectorAll('a').forEach(a => {
-          a.classList.add('text-gray-200', 'hover:text-white');
-          a.classList.remove('text-gray-800', 'hover:text-ice-blue-500');
-      });
-
-      hx.classList.add('from-ice-blue-100','to-ice-blue-200');
-      hx.classList.remove('from-ice-blue-400','to-ice-blue-600');
-      langBtn.classList.add('bg-white', 'text-ice-blue-500');
-      langBtn.classList.remove('bg-ice-blue-500', 'text-white');
-  }
+    if (window.scrollY > 50) {
+        console.log('scrollingnggg')
+        addNavbarBackground();
+    } else if (!hamburgerIsOpen) {
+        removeNavbarBackground();
+    }
 });
